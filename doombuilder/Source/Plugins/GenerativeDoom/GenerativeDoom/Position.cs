21633d40417885c33e78d50529da855d6e63c9ef
KangNansi <evillz@hotmail.com>
2017-05-06 14:45:32 +0200

    final

---
+++ doombuilder/Source/Plugins/GenerativeDoom/GenerativeDoom/Position.cs
@@ -0,0 +1,48 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace GenerativeDoom
+{
+    public enum Direction
+    {
+        LEFT, RIGHT, UP, DOWN
+    }
+
+    class Position : IEqualityComparer<Position>
+    {
+        public int x, y;
+
+        public Position(int _x, int _y)
+        {
+            x = _x;
+            y = _y;
+        }
+
+        public bool Equals(Position a, Position b)
+        {
+            return (a.x == b.x && a.y == b.y);
+        }
+
+        public override bool Equals(Object other)
+        {
+            return (x == ((Position)other).x && y == ((Position)other).y);
+        }
+
+        public override int GetHashCode()
+        {
+            return x + y;
+        }
+
+        public int GetHashCode(Position p)
+        {
+            return p.x.GetHashCode() + p.y.GetHashCode();
+        }
+
+        static public Position operator +(Position p, Position p2)
+        {
+            return new Position(p.x + p2.x, p.y + p2.y);
+        }
+    }
+}
---
+++ doombuilder/Source/Plugins/GenerativeDoom/GenerativeDoom/TableGraph.cs
@@ -0,0 +1,79 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace GenerativeDoom
+{
+    
+    //Graphe
+    class TableGraph<T>
+    {
+        //Noeud du graphe
+        public class TableNode<T>
+        {
+            public T data;
+            public bool right = false;
+            public bool left = false;
+            public bool up = false;
+            public bool down = false;
+            public TableNode(T d)
+            {
+                data = d;
+            }
+        }
+
+        
+
+        public Dictionary<Position, TableNode<T>> nodes = new Dictionary<Position, TableNode<T>>();
+
+        public void Add(Position key, T value)
+        {
+            nodes[key] = new TableNode<T>(value);
+        }
+
+        public List<Direction> freeNearbyPosition(Position p)
+        {
+            List<Direction> ret = new List<Direction>();
+            if (isEmpty(p + new Position(-1, 0)))
+                ret.Add(Direction.LEFT);
+            if (isEmpty(p + new Position(1, 0)))
+                ret.Add(Direction.RIGHT);
+            if (isEmpty(p + new Position(0, 1)))
+                ret.Add(Direction.DOWN);
+            if (isEmpty(p + new Position(0, -1)))
+                ret.Add(Direction.UP);
+            return ret;
+        }
+
+
+
+        public bool isEmpty(Position key)
+        {
+            return !nodes.ContainsKey(key);
+        }
+
+        public Position moveDir(Position key, Direction dir)
+        {
+            if (dir == Direction.LEFT) return key + new Position(-1, 0);
+            if (dir == Direction.RIGHT) return key + new Position(1, 0);
+            if (dir == Direction.UP) return key + new Position(0, -1);
+            if (dir == Direction.DOWN) return key + new Position(0, 1);
+            return key;
+        }
+
+        public TableNode<T> this[Position key]
+        {
+            get
+            {
+                return nodes[key];
+            }
+            set
+            {
+                nodes[key] = value;
+            }
+        }
+
+
+    }
+}
